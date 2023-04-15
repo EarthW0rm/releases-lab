48f7ce1d87870d98884a5839147c37018992e1c5
marco.faustino <marco.faustino@grupoboticario.com.br>
2023-04-15 17:41:12 -0300

    ops: minor version

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    extends: ['gitmoji'],
+    extends: ['@commitlint/config-conventional'],
     rules: {
         'header-max-length': [0, 'always', 72],
         'body-min-length': [0, 'always', 10],
-        'type-enum': [0, 'always', ['chore', 'feat', 'fix', 'refactor', 'docs', 'perf', 'style', 'test', 'ops', 'revert']],
+        'type-enum': [0, 'always', [':bug:', 'chore', 'feat', 'fix', 'refactor', 'docs', 'perf', 'style', 'test', 'ops', 'revert']],
         'type-case': [0, 'always', 'lowerCase']
     }
 }
