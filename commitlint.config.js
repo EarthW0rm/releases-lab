6d4ed2655ba7315f9ac07c43a7f2789c8930dbdf
marco.faustino <marco.faustino@grupoboticario.com.br>
2023-04-15 17:04:32 -0300

    ops: alterando commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
     rules: {
         'header-max-length': [0, 'always', 72],
         'body-min-length': [0, 'always', 10],
-        'type-enum': [0, 'always', ['chore', 'feat', 'fix', 'refactor', 'docs', 'perf', 'style', 'test', 'build', 'ci', 'revert']],
+        'type-enum': [0, 'always', ['chore', 'feat', 'fix', 'refactor', 'docs', 'perf', 'style', 'test', 'ops', 'revert']],
         'type-case': [0, 'always', 'lowerCase']
     }
 }
