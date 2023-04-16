0612e19211179c7d887cc491442614848cc4f2e9
marco.faustino <marco.faustino@grupoboticario.com.br>
2023-04-16 20:31:05 -0300

    :pencil2: fix: adicionando helpers

--- .releaserc.js
+++ .releaserc.js
@@ -1,47 +1,59 @@
 const path = require('path')
 const fs = require('fs')
 
-const tplFile = path.resolve(__dirname, 'release-notes.hbs')
+const tplFile = path.resolve(__dirname, '.gitmoji/release-notes.hbs')
+const commitTemplate = path.resolve(__dirname, '.gitmoji/commit-template.hbs')
 
 module.exports = {
-  branches: [
-    "main",
-    "next",
-    "next-major",
-    {
-      "name": "beta",
-      "prerelease": true
-    },
-    {
-      "name": "alpha",
-      "prerelease": true
-    }
-  ],
-  plugins: [
-    [
-      'semantic-release-gitmoji',
-      {
-        releaseRules: {
-          patch: {
-            include: [':bento:', ':arrow_up:', ':lock:'],
-          },
+    branches: [
+        "main",
+        "next",
+        "next-major",
+        {
+            "name": "beta",
+            "prerelease": true
         },
-        releaseNotes: {
-          template: fs.readFileSync(tplFile, 'utf-8'),
+        {
+            "name": "alpha",
+            "prerelease": true
         }
-      }
     ],
-    '@semantic-release/github',
-    '@semantic-release/npm',
-    [
-      '@semantic-release/git',
-      {
-        message: [
-          ':bookmark: v${nextRelease.version} [skip ci]',
-          '',
-          'https://github.com/momocow/semantic-release-gitmoji/releases/tag/${nextRelease.gitTag}'
-        ].join('\n')
-      }
+    plugins: [
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "docs/CHANGELOG.md",
+                "changelogTitle": "# Changelog\n\nRegistro de Alterações: Mantenha-se Atualizado com as Últimas Novidades e Melhorias!"
+            }
+        ],
+        [
+            'semantic-release-gitmoji',
+            {
+                releaseRules: {
+                    patch: {
+                        include: [':bento:', ':arrow_up:', ':lock:'],
+                        partials: { commitTemplate },
+                        helpers: {
+                            datetime: function (format = 'UTC:yyyy-mm-dd') {
+                              return dateFormat(new Date(), format)
+                            }
+                        },
+                    },
+                },
+                releaseNotes: {
+                    template: fs.readFileSync(tplFile, 'utf-8'),
+                }
+            }
+        ],
+        '@semantic-release/github',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                message: [
+                    ':bookmark: chore: release v${nextRelease.version} [skip ci]'
+                ].join('\n')
+            }
+        ]
     ]
-  ]
 }
